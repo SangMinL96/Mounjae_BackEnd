122277005dcacd5c34c27fbac8b5dd20ca3a7862
SangMin96 <hwon3794@gmail.com>
2021-01-18 23:24:00 +0900

    회원가입 pw 암호화, 방생성 스키마 및 로직, 모든방 리스트
    회원가입 pw 암호화
    방생성 스키마 및 로직
    모든방 리스트

--- src/api/Room/room.js
+++ src/api/Room/room.js
@@ -1,38 +1,36 @@
-
 export default {
-    Mutation:{
-        saveRoom:async(_,args,{request,query})=>{
-            try{
-
-                const param=args.param;
-                console.log(param)
-                const result =await query('room','saveRoom',{...param,masterid:request.user.id});
-            
-               console.log(result)
-               if(!result){
-                   return {rslt: 'NG', text:"SQL_NG"}
-               }else{
-                   return {rslt: 'OK', text:"SQL_OK"} 
-               }
-            }catch(err){
-// console.log(err)
-            }
-            }
+  Mutation: {
+    saveRoom: async (_, args, { request, query }) => {
+      try {
+        const param = args.param;
+        const result = await query("room", "saveRoom", {
+          ...param,
+          masterid: request.user.id,
+        });
+        if (!result) {
+          return { rslt: "NG", text: "SQL_NG" };
+        } else {
+          await query("room", "saveRoomUser", {roomId:result.insertId,userId:request.user.id});
+          return { rslt: "OK", text: "SQL_OK" };
+        }
+      } catch (err) {
+        // console.log(err)
+      }
     },
-    Query:{
-        getRoomList:async(_,args,{request,query})=>{
-            try{
-                const result =await query('room','getRoom',{});
-            
-                console.log(result[0])
-                 if(!result[0]){
-                return {id:"null",token:""}
-            }else{
-                return [result[0]]
-            }
-            }catch(err){
-                  // console.log(err)
-            }
-            }
-    }
-}
+  },
+  Query: {
+    getRoomList: async (_, args, { request, query }) => {
+      try {
+        const result = await query("room", "getRoom", {});
+        if (!result[0]) {
+          return null;
+        } else {
+          return result;
+        }
+      } catch (err) {
+        // console.log(err)
+      }
+    },
+  },
+  
+};
